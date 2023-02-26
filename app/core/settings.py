9da4ab5c522557fa8804b9589f8e50d0f6de163d
FernandoCelmer <email@fernandocelmer.com>
2023-02-26 03:37:22 -0300

    ⚙️  NO-TASK - Settings

---
+++ app/core/settings.py
@@ -0,0 +1,20 @@
+from os import environ, path
+
+
+def set_up():
+    """Sets up configuration for the app
+    """
+
+    try:
+        from dotenv import load_dotenv
+        load_dotenv()
+    except:
+        pass
+
+    config = {
+        "DEBUG": bool(environ.get("DEBUG", 1)),
+        "DATABASE_URL": environ.get("DATABASE_URL", "sqlite:///./sql_app.db"),
+        "SECRET_KEY": environ.get("SECRET_KEY", "Tor"),
+        "BASE_DIR": path.abspath(path.dirname(__file__))
+    }
+    return config
