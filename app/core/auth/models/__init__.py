ae4ea2a4b63940ffadb24e4ea33bb2e0458db7ae
FernandoCelmer <email@fernandocelmer.com>
2023-02-26 03:39:01 -0300

    ⚙️  NO-TASK - Auth API

---
+++ app/core/auth/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'AuthUser',
+    'AuthGroup',
+    'AuthUserGroup'
+]
+
+
+from app.core.auth.models.auth_user import AuthUser
+from app.core.auth.models.auth_group import AuthGroup
+from app.core.auth.models.auth_user_groups import AuthUserGroup
---
+++ app/core/auth/models/auth_user.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    String,
+    Integer)
+from app.core.database import Base
+from app.core.controller import BaseController
+
+
+class AuthUser(Base):
+    """Model Auth Users
+    """
+
+    __tablename__ = "auth_user"
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(75), unique=True)
+    username = Column(String(45), unique=True)
+    password = Column(String(100), unique=True)
+    first_name =  Column(String(100))
+    last_name =  Column(String(100))
+    is_staff = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+    created_date = Column(DateTime, default=datetime.utcnow)
+    update_date = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class ControllerAuthUser(BaseController):
+
+    def __init__(self, db = None):
+        super().__init__(db)
+        self.model_class = AuthUser
